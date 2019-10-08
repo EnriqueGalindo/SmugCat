7e3e0af35cc0fbaca32ea8788aa37f8ea3a90d69
Jake Hershey <joehershey45@gmail.com>
2019-10-07 22:56:07 -0700

    Added basic profiles

--- src/frontend/pages/App/index.js
+++ src/frontend/pages/App/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
-import Home from '../Home';
-import Login from '../Login';
-import './App.css';
-import withAuth from '../../components/withAuth';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import Route from "react-router-dom/Route";
+import { setGlobal } from "reactn";
+import Switch from "react-router-dom/Switch";
+import Home from "../Home";
+import Login from "../Login";
+import "./App.css";
+import withAuth from "../../components/withAuth";
+import StoreProfile from "../StoreProfile";
+import EditProfile from "../../components/EditProfile";
+import CustomerProfile from "../CustomerProfile";
+
+setGlobal({
+  storeUser: [
+    {
+      username: "bob",
+      displayName: "bob",
+      pictureLocation:
+        "https://crystal-cdn1.crystalcommerce.com/photos/6509981/en_skzbNgJr7v.png",
+      borderPictureLocation:
+        "https://dlair.net/austin/wp-content/uploads/2017/05/mtg-banner-730x280.jpg",
+      about: "pew pew"
+    }
+  ],
+
+  customerUser: [
+    {
+      username: "boop",
+      displayName: "boop",
+      pictureLocation:
+        "https://crystal-cdn1.crystalcommerce.com/photos/6509981/en_skzbNgJr7v.png",
+      borderPictureLocation:
+        "https://dlair.net/austin/wp-content/uploads/2017/05/mtg-banner-730x280.jpg",
+      about: "you must construct additional pylons"
+    }
+  ]
+});
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Login} />
+    <Route exact path="/store" component={StoreProfile} />
+    <Route exact path="/customer" component={CustomerProfile} />
     <Route exact path="/home" component={withAuth(Home)} />
+    <Route exact path="/edit" component={EditProfile} />
   </Switch>
 );
 
---
+++ src/frontend/pages/CustomerProfile/index.js
@@ -0,0 +1,71 @@
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { useGlobal } from "reactn";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardFooter,
+  Button
+} from "shards-react";
+
+export default function CustomerProfile() {
+  const [customerUser, setCustomerUser] = useGlobal("customerUser");
+
+  return (
+    <Card
+      style={{
+        textAlign: "center",
+        textShadow: "text-shadow: 3px 5px 2px #474747",
+        color: "white"
+      }}
+    >
+      <CardImg
+        style={{ height: "150px" }}
+        variant="top"
+        src={customerUser.borderPictureLocation}
+      />
+      <CardHeader style={{ backgroundColor: "#191970", fontSize: "40px" }}>
+        {customerUser.displayName}
+      </CardHeader>
+      <CardImg
+        style={{ width: "300px" }}
+        variant="top"
+        src={customerUser.pictureLocation}
+      />
+
+      <CardBody style={{ backgroundColor: "#5E2D79" }}>
+        About:
+        {customerUser.about}
+      </CardBody>
+
+      <div
+        style={{
+          backgroundColor: "#5E2D79",
+          display: "flex",
+          justifyContent: "space-around"
+        }}
+      >
+        <NavLink to="/edit">
+          <Button>Edit</Button>
+        </NavLink>
+        <Button
+        // onClick=
+        // {handleDeleteUser}
+        >
+          Delete
+        </Button>
+
+        <div
+        // onSubmit={this.handleUploadPicture}
+        >
+          <div name="picture" type="file" />
+          <Button type="submit">Upload Picture</Button>
+        </div>
+        <div />
+      </div>
+    </Card>
+  );
+}
