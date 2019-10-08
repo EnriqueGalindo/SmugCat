89f006b4e05fbd8631ea69a66257322c5711128b
Enrique Galindo <egalindo@protonmail.com>
2019-10-08 18:39:57 +0000

    Revert "Merge branch 'adding-cards-to-user-data' into 'master'"
    This reverts merge request !3

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,76 +1,34 @@
-import bcrypt from "bcrypt";
-import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
 
 const saltrounds = 10;
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  cards: {
-    type: [
-      {
-        cardName: { type: String, required: true, unique: true },
-        locations: {
-          type: [
-            {
-              location: { type: String, required: true, unique: true },
-              quantity: { type: Number, required: true }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+})
+
+UserSchema.pre('save', function(next) {
+    if(this.isNew || this.isModified('password')) {
+        const document = this;
+        bcrypt.hash(document.password, saltrounds, function(err, hashedPassword) {
+            if (err) next(err);
+            else {
+                document.password = hashedPassword;
+                next();
             }
-          ],
-          required: true
-        }
-      }
-    ],
-    required: true,
-    default: []
-  }
-  /*
-        cards data structure
-        Array<{ cardName: String, locations: Array<{ location: String, quantity: Number }>}>
-
-        [{ cardName: "SameCard", locations: [{ location: "B2", quantity: 3 }] }]
-        data structure -> UI
-        
-    */
-
-  /* 
-        register endpoint
-        request body: { email: String, password: String }
-        response body: nothing (200 status code OK)
-
-        login endpoint 
-        request body: { email: String, password: String }
-        response body: { email: String, password: String, cards: [{ cardName: "SameCard", locations: [{ location: "B2", quantity: 3 }] }] }
-
-        // POST - addCard endpoint
-        // request body: { cardName: "SameCard", location: "B2" }
-        // response body: { cardName: "SameCard", locations: [{ location: "B2", quantity: 4 }] }
-
-        // DELETE - removeCard endpoint
-    */
-});
-
-UserSchema.pre("save", function(next) {
-  if (this.isNew || this.isModified("password")) {
-    const document = this;
-    bcrypt.hash(document.password, saltrounds, function(err, hashedPassword) {
-      if (err) next(err);
-      else {
-        document.password = hashedPassword;
+        })
+    } else {
         next();
-      }
-    });
-  } else {
-    next();
-  }
-});
+    }
+})
 
 UserSchema.methods.isCorrectPassword = function(password, callback) {
-  bcrypt.compare(password, this.password, (err, same) => {
-    if (err) callback(err);
-    else callback(err, same);
-  });
-};
+    bcrypt.compare(password, this.password, (err, same) => {
+        if (err) callback(err)
+        else callback(err, same)
+    })
+}
 
-let User = mongoose.model("User", UserSchema);
+let User = mongoose.model('User', UserSchema);
 export default User;
--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -1,18 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export const MONGO_URI = "mongodb://localhost/TcgInventory";
+export const MONGO_URI = 'mongodb://localhost/coachConnect';
 
-export const connect = () =>
-  mongoose.connect(
-    MONGO_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    },
-    err => {
-      if (err) throw err;
-      else console.log("connected to database: " + MONGO_URI);
+export const connect = () => mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, err => {
+        if (err) throw err;
+        else console.log('connected to database: ' + MONGO_URI)
     }
-  );
+)
 
-export { default as User } from "./User";
+export {default as User} from './User';
