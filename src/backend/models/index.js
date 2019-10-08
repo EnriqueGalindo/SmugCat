feec5747ed98b06b572adc9b43b0e47b14027729
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-10-08 14:11:33 -0400

    expand user schema and notes

--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -1,14 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-export const MONGO_URI = 'mongodb://localhost/coachConnect';
+export const MONGO_URI = "mongodb://localhost/TcgInventory";
 
-export const connect = () => mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, err => {
-        if (err) throw err;
-        else console.log('connected to database: ' + MONGO_URI)
+export const connect = () =>
+  mongoose.connect(
+    MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    err => {
+      if (err) throw err;
+      else console.log("connected to database: " + MONGO_URI);
     }
-)
+  );
 
-export {default as User} from './User';
+export { default as User } from "./User";
