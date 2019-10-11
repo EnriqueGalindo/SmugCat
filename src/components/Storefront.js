4f7a8f215589b08993d194468e6f39fc7bb38fb9
dsbarnes <derekb0147@gmail.com>
2019-10-11 13:26:54 -0400

    mocked up the jumbotron

--- src/components/Storefront.js
+++ src/components/Storefront.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
-import { Button } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 // import { connect } from "react-redux";
 
-
+//I'm sure there is a way to do this with import
+//but I've gotten it to work this way...
+const myModule = require('./imgString')
+const imgString = myModule.imgString
 
 export default class Storefront extends Component {
     render() {
         return (
         <React.Fragment>
-            <Button>Yay!</Button>
-            <p>This is your Storefront</p>
-            <p>This is your Storefront</p>
-            <p>This is your Storefront</p>
+            <Container class="container" style={{border: "1px solid black", minHeight: "420px"}}>
+            <Row>
+                <Col md={4}>
+                    <Image src={imgString} roundedCircle />
+                </Col>
+                <Col md={4}>
+                    <p>This will be blank.</p>
+                </Col>
+                <Col md={4}>
+                    <p>This will be a search</p>
+                </Col>
+            </Row>
+            </Container>
+
         </React.Fragment>
     );
     }
