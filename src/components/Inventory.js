885e4661b89e02f0f47d1896fe4d11ddf2687e8f
Enrique Galindo <egalindo@protonmail.com>
2019-10-14 17:40:34 -0400

    added navbar link and added it to inventory

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -4,6 +4,15 @@ import { getCards } from "../actions"
 import Box1 from "./Box1"
 import CustardModal1 from "./CustardModal1"
 import CustardModal2 from "./CustardModal2"
+import { Container, Row, Col, 
+  Image, Navbar, NavDropdown, Nav, 
+  Jumbotron, InputGroup, FormControl 
+  } from 'react-bootstrap';
+import {
+  Button,
+  Card, CardHeader, CardTitle, CardImg, CardBody, CardFooter,
+  FormInput, FormRadio
+  } from "shards-react";
 
 // import {} from "shards-react";
 // import {} from 'react-bootstrap';
@@ -20,6 +29,23 @@ class Inventory extends Component {
   render() {
     return (
       <React.Fragment>
+            <Jumbotron style={{margin: "0px"}}>
+                <h1>Hello, world!</h1>
+                <p>This is a simple hero unit, a simple jumbotron-style component for calling 
+                extra attention to featured content or information.</p>
+            </Jumbotron>
+            <Navbar>
+                <Nav className="mr-auto">
+                    <Nav.Link href="*">Upcoming</Nav.Link>
+                    <Nav.Link href="*">Near Me</Nav.Link>
+                    <Nav.Link href="*">All Events</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="/inventory">My Inventory</Nav.Link>
+                    <Nav.Link href="/profile/search">Search Users</Nav.Link>
+                    <Nav.Link href="/">Logout</Nav.Link>
+                </Nav>
+            </Navbar>
         <div>
         {/*this is a modal for box 1 */}
         <CustardModal1/>
