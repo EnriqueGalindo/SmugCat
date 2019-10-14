0f328beb37712041113d1c4d1c714f12c92c34f0
Jake Hershey <joehershey45@gmail.com>
2019-10-14 07:15:57 -0700

    Search users

---
+++ src/components/SearchedUser.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class SearchedUser extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SearchedUser;
