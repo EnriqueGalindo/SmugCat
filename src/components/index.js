41a1f766b864bd4a5532404b8489d4bf79a5e157
Patrick Martin <pmartt10@gmail.com>
2019-10-16 13:58:42 -0400

    added events fetch to Profile, some styling.. still need to improve the banner

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as LoginPage } from "./LoginPage";
 export { default as Inventory } from "./Inventory";
-export { default as Profile } from "./Profile";
+export { default as Profile } from "./Profile"; //{default as Profile}
 export { default as Storefront } from "./Storefront";
 export { default as TestComponent } from "./TestComponent";
